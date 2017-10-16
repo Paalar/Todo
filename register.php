347060e14a6440260f8b39f85ec333e43b59e322
Pål Larsen <paaledward@gmail.com>
2017-10-16 17:46:49 +0200

    test

--- register.php
+++ register.php
@@ -3,8 +3,9 @@ require("init.php");
 
 $username = "Pål";
 $password = "12345";
+$email = "test@test.com";
 
-$sql_query = "insert into users values('$name', '$password');";
+$sql_query = "insert into users values('$name', '$password', '$email');";
 
 if (mysqli_query($con, $sql_query)) {
   echo("<p>
