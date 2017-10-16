f18dec87d4594ef44a6557f7085f792f66cda7b0
Pål Larsen <paaledward@gmail.com>
2017-10-16 19:01:10 +0200

    Think the php for regiser should work with android studio

--- login.php
+++ login.php
@@ -3,13 +3,15 @@ require "init.php";
 $username = "Pål";
 $password = "12345";
 
-$sql_query = "select name from users where username = '$username' and password = '$password';";
+$sql_query = "select username from users where username = '$username' and password = '$password';";
 $result = mysqli_query($con, $sql_query);
 
 if ($result->num_rows > 0) {
 $row = mysqli_fetch_assoc($result);
 $name = $row["name"];
-echo("Welcome ".$name);
+echo("<p>
+".$name."
+</p>");
 } else {
   echo("No info is available");
 }
--- register.php
+++ register.php
@@ -1,11 +1,11 @@
 <?php
 require "init.php";
 
-$username = "Pål";
-$password = "12345";
-$email = "test@test.com";
+$username = $_POST["username"];
+$password = $_POST["password"];
+$email = $_POST["email"];
 
-$sql_query = "insert into users(username, password) values('$username', '$password');";
+$sql_query = "insert into users(username, password, email) values('$username', '$password', '$email');";
 
 if (mysqli_query($con, $sql_query)) {
   echo("<p>
