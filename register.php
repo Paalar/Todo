878cac2ca85e1c0a46e7f8a4af2a82c84760eadc
Pål Larsen <paaledward@gmail.com>
2017-10-16 17:55:23 +0200

    Fixed php not working

--- register.php
+++ register.php
@@ -1,11 +1,11 @@
 <?php
-require("init.php");
+require "init.php";
 
 $username = "Pål";
 $password = "12345";
 $email = "test@test.com";
 
-$sql_query = "insert into users values('$name', '$password', '$email');";
+$sql_query = "insert into users(username, password) values('$name', '$password');";
 
 if (mysqli_query($con, $sql_query)) {
   echo("<p>
